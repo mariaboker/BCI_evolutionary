24d9c14e4ff4ddf0f50d1dc2a3452d13fc111e91
Maria <mariaboker@hotmail.com>
2019-12-01 11:12:39 -0300

    Update fit.m

--- fit.m
+++ fit.m
@@ -1,21 +1,16 @@
-function fitness = fit(populacao)
+function fitness = fit(H, vrotulos, populacao)
 
-	sujeito = '123223';
-
-    % pre alocacao de vetores para maior velocidade
-    %fitness = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-    %EQM_pop = [1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000];
-    
+    global EQM_pop;
+    global ErroIt;
 	[Nind,~] = size(populacao);
 
 	for kk = 1:Nind
-        kk
+        
 		individuo = populacao(kk,:);
 		
 		% Conversao de codificacao binaria para ID de eletrodos
 		eletrodos = [];
         nEletr = 16;
-        %[~, nEletr] = size(individuo);
         
 		for u = 1:nEletr
 			if individuo(u) == 1
@@ -24,13 +19,15 @@ function fitness = fit(populacao)
         end
 
 
-        [H, vrotulos] = trataSinais(sujeito);
+        %%[H, vrotulos] = trataSinais(sujeito); CHAMAR NO GENETICO
 
 		W_atual = treinamento(H, vrotulos, eletrodos); 				 %% passa os eletrodos da populacao para treinamento
-		[~, EQM_atual] = teste(H, vrotulos, eletrodos, W_atual);		 %% testa com aquela populacao
+		[ErroSessao, EQM_atual] = teste(H, vrotulos, eletrodos, W_atual);		 %% testa com aquela populacao
 
-		EQM_pop(kk) = EQM_atual; 	%% vetor guarda os EQM de cada individuo da populacao 
-
+		EQM_pop(kk) = EQM_atual; 	%% vetor guarda os EQM de cada individuo da populacao        
+        ErroSessao_pop(kk) = ErroSessao;
+        ErroIt = mean(ErroSessao_pop);
+        
 		% calcula a fitness do individuo
 		fitness(kk) = (1/(EQM_atual + 1));
 
