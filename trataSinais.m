793c84b9f133ea3d7d30f5a221e73908caaca0c4
Maria <mariaboker@hotmail.com>
2019-12-01 11:13:17 -0300

    Update trataSinais.m

--- trataSinais.m
+++ trataSinais.m
@@ -7,17 +7,16 @@ function [H, vrotulos] = trataSinais(sujeito)
 
     H = [];
     vrotulos = [];
-    Njanelas = 8;
 
     num_eletr_orig = 16;
 
     % Treinar com 70% das sessoes definidas aleatoriamente
     for ss = 1:nsess_total
-        ss
+        %ss
         IDSessao = num2str(ss);
         %%%%% PRE-PROCESSAMENTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % pre-processa mao esquerda
-        arquivo = ['smr_training_session_' IDSessao '_leftHand_subject_' sujeito '_20161031_lrh.mat'];
+        arquivo = ['smr_training_session_' IDSessao '_leftHand_subject_' sujeito '_20161107_lrh.mat'];
         load(arquivo);
 
         sessao = rawData';
@@ -31,7 +30,7 @@ function [H, vrotulos] = trataSinais(sujeito)
 
 
         % pre processa mao direita
-        arquivo = ['smr_training_session_' IDSessao '_rightHand_subject_' sujeito '_20161031_lrh.mat'];
+        arquivo = ['smr_training_session_' IDSessao '_rightHand_subject_' sujeito '_20161107_lrh.mat'];
         load(arquivo);
 
         sessao = rawData';
@@ -54,26 +53,28 @@ function [H, vrotulos] = trataSinais(sujeito)
             atributosE = [];                                                        % cria um vetor de atributos para cada janela
             atributosD = [];     
 
-            for e = 1:length(num_eletr_orig)                                       % para cada um dos eletrodos, cria-se um elemento do vetor de atributos da janela atual
+            for e = 1:(num_eletr_orig)    
                 
                 inicio = 1 + (j - 1) * 256 * 1;                           % indice da primeira amostra da janela
                 fim = inicio + 256 * 3 - 1;                                 % indice da ultima amostra da janela
-                
+
                 janela_esq = dados_esq(e,inicio:fim);                                  % vetor com as amostras selecionadas formando a janela com (Famost * Tjanela) elementos
-                janela_dir = dados_esq(e,inicio:fim);
-                
+                janela_dir = dados_dir(e,inicio:fim);
+
                 [PxxE,FE] = pwelch(janela_esq, [], [], [1:20], 256);                     % aplicacao do metodo de P Welch na janela atual
-                [PxxD,FD] = pwelch(janela_dir, [], [], [1:20], 256);  
-                
+                [PxxD,FD] = pwelch(janela_dir, [], [], [1:20], 256); 
+
                 atrib_welch_esq = [sum(PxxE(8:12)), sum(PxxE(13:16)), sum(PxxE(17:20))]; % cada atributo eh a soma das potencias de cada uma das bandas escolhidas
                 atrib_welch_dir = [sum(PxxD(8:12)), sum(PxxD(13:16)), sum(PxxD(17:20))];                                                                     % no caso usamos as bandas de 8 a 12 Hz, de 13 a 16 Hz e de 17 a 20 Hz
-                
+
                 atributosE = [atributosE, atrib_welch_esq];
                 atributosD = [atributosD, atrib_welch_dir];                          % concatenacao dos atributos do PWelch do eletrodo atual com os dos outros eletrodos da janela atual
+
             end
 
             Hesq(j,:) = [atributosE, 1];                                           % acrescenta-se um vetor coluna de valor '1' na ultima coluna da matriz                              
-            Hdir(j,:) = [atributosD, 1]; 
+            Hdir(j,:) = [atributosD, 1];
+           
         end
 
 
@@ -89,9 +90,9 @@ function [H, vrotulos] = trataSinais(sujeito)
         end
 
         % concatena-se Hsessao da sessao atual com a H final > tamanho final = 160 linhas
-        H = vertcat(H, Hsessao);                              
+        H = vertcat(H, Hsessao);    
         
         %concatena-se vrotulos_sessao da sessao atual com vrotulos final
-        vrotulos = vertcat(vrotulos, vrotulos_sessao)
+        vrotulos = vertcat(vrotulos, vrotulos_sessao);
 
     end
