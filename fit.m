6c53f296338a8bb5b19621e4fdb9fd5564c33b95
Maria <mariaboker@hotmail.com>
2019-12-02 20:26:16 -0300

    Update fit.m

--- fit.m
+++ fit.m
@@ -1,7 +1,6 @@
-function fitness = fit(H, vrotulos, populacao)
+function [fitness, Erropop] = fit(H, vrotulos, populacao)
 
     global EQM_pop;
-    global ErroIt;
 	[Nind,~] = size(populacao);
 
 	for kk = 1:Nind
@@ -13,23 +12,22 @@ function fitness = fit(H, vrotulos, populacao)
         nEletr = 16;
         
 		for u = 1:nEletr
-			if individuo(u) == 1
+			if (individuo(u) == 1)
 				eletrodos = [eletrodos u];
             end
         end
-
-
-        %%[H, vrotulos] = trataSinais(sujeito); CHAMAR NO GENETICO
 
 		W_atual = treinamento(H, vrotulos, eletrodos); 				 %% passa os eletrodos da populacao para treinamento
 		[ErroSessao, EQM_atual] = teste(H, vrotulos, eletrodos, W_atual);		 %% testa com aquela populacao
 
 		EQM_pop(kk) = EQM_atual; 	%% vetor guarda os EQM de cada individuo da populacao        
-        ErroSessao_pop(kk) = ErroSessao;
-        ErroIt = mean(ErroSessao_pop);
-        
+        Erropop(kk) = ErroSessao;%% vetor guarda os erros de cada individuo da populacao        
+
 		% calcula a fitness do individuo
 		fitness(kk) = (1/(EQM_atual + 1));
 
-	end
+    end
+    
+    ele = ID(eletrodos)
 end
+
